78f8401e1e7c7d7235999e0e76420a205309171d
张亚飞 <672233580@qq.com>
2022-08-09 14:56:26 +0800

    Initial Commit

---
+++ ToolBarAnimation0809/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ToolBarAnimation0809
+//
+//  Created by 张亚飞 on 2022/8/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToolBarAnimation0809/ToolBarAnimation0809App.swift
@@ -0,0 +1,17 @@
+//
+//  ToolBarAnimation0809App.swift
+//  ToolBarAnimation0809
+//
+//  Created by 张亚飞 on 2022/8/9.
+//
+
+import SwiftUI
+
+@main
+struct ToolBarAnimation0809App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
